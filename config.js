ac7289652fd36b3516870737508925471d785266
duchieu190904 <90527269+duchieu190904@users.noreply.github.com>
2021-09-13 08:12:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Ngọc babe à!',
+    introTitle: 'Quyên babe à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
